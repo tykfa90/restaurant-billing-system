eb6c3c2b2abf18a1a91e1562c07d4ed5ef8da5f1
Konstanty Białek <tykfa90@gmail.com>
2020-02-04 11:27:38 +0100

    Initial commit

---
+++ src/main/java/tykfa90/restaurantbillingsystem/RestaurantBillingSystemApplication.java
@@ -0,0 +1,13 @@
+package tykfa90.restaurantbillingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBillingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBillingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tykfa90/restaurantbillingsystem/RestaurantBillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tykfa90.restaurantbillingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBillingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
